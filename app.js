399f97cca4509b61d11fb7cb5fe56214746e7307
Vadym Nemchenko <westa.nvv@gmail.com>
2017-06-22 23:41:48 +0300

    Added environment variables, mongo package

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ const path = require('path');
 const logger = require('morgan');
 const bodyParser = require('body-parser');
 
+require('dotenv').config();
+
 const index = require('./routes/index');
 const users = require('./routes/users');
 
@@ -12,6 +14,11 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 // app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(function(req, res, next) {
+  console.log(process.env.DB_HOST);
+  next();
+});
 
 app.use('/', index);
 app.use('/users', users);
