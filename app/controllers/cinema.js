9beac394bbc2c88be20e7fc5df3e38d1fa51ba4e
Vadym Nemchenko <westa.nvv@gmail.com>
2017-06-30 10:43:53 +0300

    Added base functionality for cinema model

--- app/controllers/cinema.js
+++ app/controllers/cinema.js
@@ -1,3 +1,4 @@
+const mongoose = require('mongoose');
 const Cinema = require('../models/Cinema');
 
 module.exports = {
@@ -11,19 +12,23 @@ function getAll(req, res, next) {
   Cinema.find({}).then((data, err) => {
     if (err) return next(err);
 
-    next({ttt: 3});
-    // res.send(data);
-  });
-}
-
-function getElement(req, res, next) {
-  Cinema.find({}).then((data, err) => {
-    if (err) return next(err);
-
     res.send(data);
   });
 }
 
+function getElement(req, res, next) {
+  //Check valid ObjectId
+  if (!mongoose.Types.ObjectId.isValid(req.params.id)) return res.sendStatus(404);
+
+  Cinema.findById(req.params.id).then((data, err) => {
+    if (err) return next(err);
+    if (!data) {
+      res.sendStatus(404);
+    } else {
+      res.json(data);
+    }
+  });
+}
 
 function add(req, res, next) {
   Cinema.create({
@@ -32,19 +37,18 @@ function add(req, res, next) {
   }, (err, data) => {
     if (err) return next(err);
 
-    res.json(data);
+    res.json({
+      _id: data._id
+    });
   });
 }
 
 function update(req, res, next) {
-  // Cinema.update({
-  //   _id: req.body.name || 'New cinema',
-  //   address : req.body.address || '',
-  // }, {
-  //
-  // }, (err, data) => {
-  //   if (err) return next(err);
-  //
-  //   res.json(data);
-  // });
+  Cinema.update({
+    _id: req.params.id,
+  }, { $set: req.body }, (err, data) => {
+    if (err) return next(err);
+
+    res.send();
+  });
 }
